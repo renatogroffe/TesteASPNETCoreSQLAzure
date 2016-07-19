172a4156f14e1c20b28d60e43b756b99de969c98
Renato Groffe <renato.groffe@yahoo.com.br>
2016-07-19 00:48:04 -0300

    Add project files.

---
+++ src/TesteASPNETCoreSQLAzure/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TesteASPNETCoreSQLAzure.Data;
+
+namespace TesteASPNETCoreSQLAzure.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ExemploContext _context;
+
+        public HomeController(ExemploContext context)
+        {
+            this._context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(
+                _context.Capitais.OrderBy(c => c.Estado).ToList());
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/TesteASPNETCoreSQLAzure/Data/ExemploContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TesteASPNETCoreSQLAzure.Models;
+
+namespace TesteASPNETCoreSQLAzure.Data
+{
+    public class ExemploContext : DbContext
+    {
+        public DbSet<Capital> Capitais { get; set; }
+
+        public ExemploContext(DbContextOptions<ExemploContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Capital>().HasKey(c => c.SiglaEstado);
+        }
+    }
+}
